83b000cb6a7f5f5b85a4ada4138c12ef8e6b6090
Rick Koster <ngf.koster@outlook.com>
2017-07-01 10:01:24 +0200

    allow error and meta property on actions

--- src/action.ts
+++ src/action.ts
@@ -1,8 +1,19 @@
 import * as Redux from 'redux';
 
+export interface ActionError {
+    readonly message: string;
+    readonly [properties: string]: any;
+}
+
+export interface ActionMetadata {
+    readonly [properties: string]: any;
+}
+
 export interface Action<T> extends Redux.Action {
     readonly type: string;
     readonly payload: T;
+    readonly error?: ActionError;
+    readonly meta?: ActionMetadata;
 }
 
 export interface TypedAction<T extends string, P> extends Action<P> {
