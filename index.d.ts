77ce0163625b319d4ea0d05c6c6c38b87e306a99
Rick Koster <ngf.koster@outlook.com>
2017-08-20 22:09:28 +0200

    Restore index file

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,2 @@
 
-export * from './dist/models/action-creator';
-export * from './dist/models/action';
-export * from './dist/models/reducer';
-export * from './dist/action-factory';
-export * from './dist/combine-reducers';
-export * from './dist/create-reducer';
-export * from './dist/create-store';
-export * from './dist/reducer-builder';
-export * from './dist/store';
+export * from './dist'
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+
+export * from './models/action-creator';
+export * from './models/action';
+export * from './models/reducer';
+export * from './action-factory';
+export * from './combine-reducers';
+export * from './create-reducer';
+export * from './create-store';
+export * from './reducer-builder';
+export * from './store';
